9e8bb4c4eec106138b37f68e456bc47710cd0a06
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-06-06 17:07:50 -0400

    Update securitypolicy.ps1

--- securitypolicy.ps1
+++ securitypolicy.ps1
@@ -5,7 +5,7 @@
 param (
     [Parameter(Mandatory = $True)][string]$vip,  # the cluster to connect to (DNS name or IP)
     [Parameter(Mandatory = $True)][string]$username,  # username (local or AD)
-    [Parameter(Mandatory = $True)][string]$password,  # local or AD domain password
+    [Parameter(Mandatory = $True)][string]$password  # local or AD domain password
 )
 
 # source the cohesity-api helper code
