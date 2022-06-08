19b4074df1358fa02d1dc0b918acd197c2d0b346
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-06-08 17:42:36 -0400

    Update azureblob.ps1

--- azureblob.ps1
+++ azureblob.ps1
@@ -1,8 +1,15 @@
+#$scriptName = 'azureblob'
+#$repoURL = 'https://raw.githubusercontent.com/cohesity-academy/labscripts/main'
+#(Invoke-WebRequest -Uri "$repoUrl/$scriptName.ps1").content | Out-File "$scriptName.ps1"; (Get-Content "$scriptName.ps1") | Set-Content "$scriptName.ps1"
+#(Invoke-WebRequest -Uri "$repoUrl/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+#./azureblob.ps1 -vip cohesity-a.cohesitylabs.az -username admin -password cohesity123 -Name "CoolBlob" -tierType "kAzureTierCool" -StorageAccountName "cool23954073" -StorageAccessKey "4fHIXz/nRiSU7NWTOh2umMGE6KCsyOu/oQ5AfI28RorIMMzVJa5LBOgCy1MHA5nvwIfPjoYKJjGB+ASt2hAYPQ==" -bucketName "cohesitystorage"
+
+
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
     [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
-    [Parameter()][string]$domain = 'local', #local or AD domain
+    [Parameter(Mandatory = $True)][string]$password,
     [Parameter(Mandatory = $True)][string]$tierType, #kAzureTierCool, kAzureTierHot, kAzureTierArchive
     [Parameter(Mandatory = $True)][string]$name, #Name of Blob
     [Parameter(Mandatory = $True)][string]$storageAccountName, #Storage Container Name
@@ -17,7 +24,7 @@ param (
 ### authenticate
 apiauth -vip $vip -username $username -domain $domain -password $password
 
-$myObject = @{
+    $myObject = @{
     "compressionPolicy" = "kCompressionLow";
     "config" = @{
                    "azure" = @{
@@ -45,6 +52,6 @@ $myObject = @{
     "isForeverIncrementalArchiveEnabled" = $false;
     "externalTargetType" = "kAzure";
     "_title" = "standard";
-    "_encryptionEdited" = $true
+    "_encryptionEdited" = $true;
 }
 $null = api post vaults $myObject
