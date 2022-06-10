944af5e746f5eac0e33df91c6b58ab55be153fac
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-06-10 16:27:31 -0500

    Update syslogvalidation.ps1

--- syslogvalidation.ps1
+++ syslogvalidation.ps1
@@ -16,10 +16,9 @@ apiauth -vip $vip -username $username -password $password -quiet
 
 $objectssyslogs = api get -v2 syslogs
 
-If ($objectssyslogs.server -eq "192.168.1.90" -and
-    $objectssyslogs.trapUser.userName -eq "cohesityV2Public" -and
-    $objectssyslogs.version -eq "kSnmpV2" -and
-    $objectssyslogs.readUser.userName -eq "cohesityV2Public") {
+If ($objectssyslogs.syslogServers.ip -eq "192.168.1.90" -and
+    $objectssyslogs.syslogServers.protocol -eq "udp" -and
+    $objectssyslogs.syslogServers.programNameList -eq "dataprotection_events") {
 Write-Host "Correct"
 }
 Else {Write-Host "Incorrect"}
