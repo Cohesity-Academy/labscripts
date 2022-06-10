a31585ef299dec3bc0eec63bd5a3b2c69ba8cb14
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-06-10 15:41:00 -0500

    Create logvalidation.ps1

---
+++ logvalidation.ps1
@@ -0,0 +1,22 @@
+### usage: ./logvalidation.ps1 -vip mycluster -username myusername -passsword 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$objects = api get /snmp/config
+
+If ($objects.cluster.AuditLogConfig.RetentionPeriodDays -eq "180") {
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
