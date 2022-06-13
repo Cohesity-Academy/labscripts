d01194c0e20f74d51cae2aecefb2d0f83b21829f
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-06-13 10:23:43 -0500

    Create dpgroupvalidation.ps1

---
+++ dpgroupvalidation.ps1
@@ -0,0 +1,23 @@
+### usage: ./dpgroupvalidation.ps1 -vip mycluster -username myusername -password 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$objectsdpgroups = api get -v2 data-protect/protection-groups
+
+If ($objectsdpgroups.protectionGroups[0].name -eq "Ransomware" -and
+    $objectsdpgroups.protectionGroups[0].name -eq "NasProtection") {
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
