dacf8232d4425f75da4ab96af3f7f402c8b0a126
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-06-13 19:29:00 -0500

    Update backupSQL.ps1

--- DSA660/backupSQL.ps1
+++ DSA660/backupSQL.ps1
@@ -1,3 +1,22 @@
+# usage: ./backupolicy.ps1 -vip mycluster -username myusername -password mypassword -Name "policyname" -frequency frequencyinminutes
+
+# process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,  # the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username,  # username (local or AD)
+    [Parameter(Mandatory = $True)][string]$password,  # local or AD domain password
+    [Parameter(Mandatory = $True)][int]$frequency,  # backup in min   
+    [Parameter(Mandatory = $True)][string]$name  # name of policy
+)
+
+# source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+# authenticate
+apiauth -vip $vip -username $username -domain $domain -password $password -quiet
+
+
 $myObject = @{
     "policyId" = "8158516650510261:1575649096260:1";
     "startTime" = @{
