9cddd631e62b2c8bf0ed68a320d6d2e714b6ab0f
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-08-12 18:55:12 -0400

    Add files via upload

---
+++ DPO660/validatedisabled.ps1
@@ -0,0 +1,22 @@
+﻿### usage: ./validation.ps1 -vip mycluster -username myusername -passsword 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$objects = api get -v2 data-protect/protection-groups
+
+If ((($objects |?{$_.protectionGroups.name -eq "WindowsBlockProtection"}).protectionGroups.isPaused) -eq "True"){
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
