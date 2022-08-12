f9450ab0073330db459edf2b799be0d8a1a5dfcb
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-08-12 17:43:21 -0400

    Add files via upload

---
+++ DPO660/validaterecovery.ps1
@@ -0,0 +1,23 @@
+﻿### usage: ./validation.ps1 -vip mycluster -username myusername -passsword 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$objects = api get -v2 data-protect/recoveries
+
+If ((($objects |?{$_.recoveries.physicalParams.objects.objectInfo.name -eq "cohesity-jump.cohesitylabs.az"}).recoveries.physicalParams.recoverFileAndFolderParams.filesAndFolders[0].absolutePath) -eq  "/C/Users/Coh-Student.COHESITYLABS/Documents/Cohesity/Images/cohesity.png"){
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
+
