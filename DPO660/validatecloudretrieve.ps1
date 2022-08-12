2542ec37351c367701cd1f17e88d39189e9e4673
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-08-12 18:54:29 -0400

    Add files via upload

---
+++ DPO660/validatecloudretrieve.ps1
@@ -0,0 +1,7 @@
+﻿$connect = Connect-CohesityCluster -Server cohesity-b.cohesitylabs.az -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "LOCAL\admin", (ConvertTo-SecureString -AsPlainText "cohesity123" -Force))
+$out = Get-CohesityProtectionJob
+
+If (($out |?{$_.Name -eq "HyperVCloudProtection"}).Name -eq "HyperVCloudProtection"){
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
---
+++ DPO660/validatereplication.ps1
@@ -0,0 +1,22 @@
+﻿### usage: ./validation.ps1 -vip mycluster -username myusername -passsword 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$objects = api get -v2 data-protect/recoveries
+
+If ($objects |?{$_.recoveries.hypervParams.objects.objectInfo.name -eq "Guest-VM-1"}){
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
