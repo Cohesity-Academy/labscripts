f1656673274c6212cd5aaab1c56a85b503c2acb9
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-08-12 17:42:31 -0400

    Update validateupgrade.ps1

--- DPO660/validateupgrade.ps1
+++ DPO660/validateupgrade.ps1
@@ -1,4 +1,4 @@
-﻿Connect-CohesityCluster -Server cohesity-a.cohesitylabs.az -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "LOCAL\admin", (ConvertTo-SecureString -AsPlainText "cohesity123" -Force))
+Connect-CohesityCluster -Server cohesity-a.cohesitylabs.az -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "LOCAL\admin", (ConvertTo-SecureString -AsPlainText "cohesity123" -Force))
 $out = get-cohesityphysicalagent
 If (($out |?{$_.Name -eq "nfs-server-1.cohesitylabs.az"}).upgradability -eq "kCurrent"){
 Write-Host "Correct"
