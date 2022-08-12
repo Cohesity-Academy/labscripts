9864407ef9991799a872f3750800dc5418049e8b
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-08-12 16:33:56 -0400

    Add files via upload

---
+++ DPO660/validateupgrade.ps1
@@ -0,0 +1,6 @@
+﻿Connect-CohesityCluster -Server cohesity-a.cohesitylabs.az -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "LOCAL\admin", (ConvertTo-SecureString -AsPlainText "cohesity123" -Force))
+$out = get-cohesityphysicalagent
+If (($out |?{$_.Name -eq "nfs-server-1.cohesitylabs.az"}).upgradability -eq "kCurrent"){
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
