6ddc57e9ccbe7a2eb90ce03f595aac7c76a09ba6
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-11-01 16:15:06 -0500

    Create validateencstorage.ps1

---
+++ SF660/validateencstorage.ps1
@@ -0,0 +1,22 @@
+### usage: ./logvalidation.ps1 -vip mycluster -username myusername -passsword 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$encstorobjects = api get viewBoxes
+
+If (($encstorobjects |?{$_.name -eq "sd-idd-ic-encrypt"}).storagePolicy.encryptionPolicy -eq "kEncryptionStrong") {
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
