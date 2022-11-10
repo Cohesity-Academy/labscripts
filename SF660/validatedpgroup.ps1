249ff2eb75d4988d27f18f1b7e6e0d9b8e48b182
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-11-10 10:45:30 -0600

    Create validatedpgroup.ps1

---
+++ SF660/validatedpgroup.ps1
@@ -0,0 +1,22 @@
+### usage: ./dpgroupvalidation.ps1 -vip mycluster -username myusername -password 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$objectsdpgroups = api get -v2 data-protect/protection-groups
+
+If ($objectsdpgroups.protectionGroups[3].name -eq "Prod-DL-Judge-Dredd") {
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
