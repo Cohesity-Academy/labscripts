da55f0e7d6e08e92b1204f44a03372fd195430c2
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-11-10 15:10:00 -0600

    Create validatetierstorage.ps1

---
+++ SF660/validatetierstorage.ps1
@@ -0,0 +1,23 @@
+### usage: ./logvalidation.ps1 -vip mycluster -username myusername -passsword 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$storage = api get viewBoxes
+$vault = api get vaults
+
+If ($storage |?{$_.name -eq "sd-idd-ic-Tier"} and ($vault |?{$_.name -eq "Az-Hot-Blob-Tier"} ) {
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
