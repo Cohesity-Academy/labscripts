e87decd85636f4b2d5a977f1b1b087b298b9494b
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2022-11-15 16:37:17 -0600

    Create validatesmbprotect.ps1

---
+++ SF660/validatesmbprotect.ps1
@@ -0,0 +1,22 @@
+### usage: ./validation.ps1 -vip mycluster -username myusername -passsword 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$jobs = api get protectionJobs
+
+If ($jobs |?{$_.name -eq "Protect_SMB_Home"}){
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
