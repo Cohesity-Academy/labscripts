dd033d90a2856c07583392afcd7f0e72f79427eb
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-02-09 12:17:16 -0600

    Create RMAN_Backup.sh

---
+++ ora660/RMAN_Backup.sh
@@ -0,0 +1,28 @@
+ORACLE_SID=CohesityDB1
+ORACLE_HOME=/u01/app/oracle/product/19.0.0/dbhome_1
+PATH=$ORACLE_HOME/bin:$PATH
+RMAN=$ORACLE_HOME/bin/rman
+TAG="$ORACLE_SID_"'data +%Y%m%d%H%M'
+LOGDIR=/home/oracle/logs/CohesityDB1
+mkdir -p $LOGDIR
+
+export ORACLE_SID ORACLE_HOME PATH
+export NLS_DATA_FORMAT='DD-MM-YYY HH24:MI:SS'
+export DATE=$(date +%Y-%m-%d-%T)
+
+LOGFILE="$LOGDIR/$(date +%Y-%m-%d-%T)_full_backup.log"
+
+echo -e "\r\n Starting full backup of $ORACLE_SID database at $DATE. \r\n"
+
+$RMAN target /msglog $LOGFILE << EOF
+
+RUN {
+
+      CONFIGURE DEVICE TYPE DISK PARALLELISM 1;
+      ALLOCATE CHANNEL f1 DEVICE TYPE DISK FORMAT='/mnt/oracle-stnd-1/%d_D_%T_$u_s%s_p%p';
+      BACKUP DATABASE PLUS ARCHIVELOG;
+      
+      }
+EOF
+
+echo - e "\r\n \r\n Full backup completed at $DATE. \r\n \r\n Job details can be found here: \$LOGFILE \r\n \r\n"
