2ce8f615a48cb33741baf9c9695c0ba50ead2c0a
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-05-12 15:40:41 -0400

    Update talabs_coh_ansible_start_pg.sh

--- AA/Ansible/talabs_coh_ansible_start_pg.sh
+++ AA/Ansible/talabs_coh_ansible_start_pg.sh
@@ -1,1 +1,1 @@
-ansible-playbook coh_ansible/talabs_coh_ansible_start_pg.yml --extra-vars "cohesity_server='172.16.3.101' cohesity_username='admin' cohesity_password='TechAccel1!' cohesity_validate_certs='false'"
+ansible-playbook coh_ansible/talabs_coh_ansible_start_pg.yml --extra-vars "cohesity_server='192.168.1.100' cohesity_username='admin' cohesity_password='cohesity123' cohesity_validate_certs='false'"
