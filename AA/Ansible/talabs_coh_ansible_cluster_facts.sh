b5840ab9a35fe7affff73be72443a207023babd3
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-05-12 15:41:20 -0400

    Update talabs_coh_ansible_cluster_facts.sh

--- AA/Ansible/talabs_coh_ansible_cluster_facts.sh
+++ AA/Ansible/talabs_coh_ansible_cluster_facts.sh
@@ -1,3 +1,3 @@
-ansible-playbook coh_ansible/talabs_coh_ansible_cluster_facts.yml --extra-vars "cohesity_server='172.16.3.101' cohesity_username='admin' cohesity_password='TechAccel1!' cohesity_validate_certs='false'"
+ansible-playbook coh_ansible/talabs_coh_ansible_cluster_facts.yml --extra-vars "cohesity_server='192.168.1.100' cohesity_username='admin' cohesity_password='cohesity123' cohesity_validate_certs='false'"
 cat coh_ansible//cohesity_facts.json
 
