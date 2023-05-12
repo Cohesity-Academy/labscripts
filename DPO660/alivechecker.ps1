0850209b73b287b13b06fa74acf9af9734487d89
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-05-11 22:20:03 -0500

    Create alivechecker.ps1

---
+++ DPO660/alivechecker.ps1
@@ -0,0 +1,29 @@
+#$scriptName0 = 'alivechecker'
+#$repoURL = 'https://raw.githubusercontent.com/cohesity-academy/labscripts/main/AA'
+#(Invoke-WebRequest -Uri "$repoUrl/$scriptName0.ps1").content | Out-File "$scriptName0.ps1"; (Get-Content "$scriptName0.ps1") | Set-Content "$scriptName0.ps1"
+#(Invoke-WebRequest -Uri "$repoUrl/cohesity-api.ps1").content | Out-File cohesity-api.ps1; (Get-Content cohesity-api.ps1) | Set-Content cohesity-api.ps1
+#.\alivechecker.ps1 -vip clusteripname -username user -password password
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, #the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$username, #username (local or AD)
+    [Parameter(Mandatory = $True)][string]$password   
+)
+
+### source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+
+### Cohesity status checker
+$synced = $false
+while($synced -eq $false){
+    Start-Sleep -Seconds 10
+    apiauth -vip $vip -username $username -password $password -quiet
+    if($AUTHORIZED -eq $true){
+        $stat = api get /nexus/cluster/status
+        if($stat.isServiceStateSynced -eq $true){
+            $synced = $true
+        }
+    }    
+}
+write-host "Cluster $vip is READY!!!"
