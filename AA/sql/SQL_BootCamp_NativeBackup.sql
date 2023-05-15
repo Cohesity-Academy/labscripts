9d85b63b68600aeb94722be4e039c20dcdee8c38
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-05-15 14:31:25 -0400

    Update SQL_BootCamp_NativeBackup.sql

--- AA/sql/SQL_BootCamp_NativeBackup.sql
+++ AA/sql/SQL_BootCamp_NativeBackup.sql
@@ -1,22 +1,22 @@
 -- Step ONE, Create the database.
-CREATE DATABASE [TALab]
+CREATE DATABASE [CALab]
  CONTAINMENT = NONE
  ON  PRIMARY 
-( NAME = N'TA-Lab_Data', FILENAME = N'D:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\TALab_Data.mdf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+( NAME = N'CA-Lab_Data', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\CALab_Data.mdf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
  LOG ON 
-( NAME = N'TA-Lab_Log', FILENAME = N'D:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\TALab_Log.ldf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+( NAME = N'CA-Lab_Log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\CALab_Log.ldf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
 
 
 -- Step TWO, Take a FULL backup of the new database.
---BACKUP DATABASE TALab TO DISK='TALab.BAK' WITH STATS=5
-BACKUP DATABASE TALab TO DISK='\\cohesity-01.talabs.local\SQLView\TALab.BAK' WITH STATS=5
+-- BACKUP DATABASE CALab TO DISK='CALab.BAK' WITH STATS=5
+BACKUP DATABASE CALab TO DISK='\\cohesity-a.cohesitylabs.az\SQLView\CALab.BAK' WITH STATS=5
 
 -- Step THREE, Take a Tx log backup of the database.
-BACKUP LOG TALab  TO DISK='\\cohesity-01.talabs.local\SQLView\TALab01.trn'
+BACKUP LOG CALab  TO DISK='\\cohesity-a.cohesitylabs.az\SQLView\CALab01.trn'
 
 -- Step FOUR, Change the context to the new database and create a table.
-USE TALab
+USE CALab
 CREATE TABLE MYLAB01 (LABNAME varchar(50))
 
 -- Step FIVE, Take a Tx log backup of the database.
-BACKUP LOG TALab  TO DISK='\\cohesity-01.talabs.local\SQLView\TALab02.trn'
+BACKUP LOG CALab  TO DISK='\\cohesity-a.cohesitylabs.az\SQLView\CALab02.trn'
