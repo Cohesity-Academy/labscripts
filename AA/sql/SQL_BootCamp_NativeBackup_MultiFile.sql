f4c899e8e3c4d17c45d54541b3e49f1404834ed3
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-05-15 14:34:03 -0400

    Update SQL_BootCamp_NativeBackup_MultiFile.sql

--- AA/sql/SQL_BootCamp_NativeBackup_MultiFile.sql
+++ AA/sql/SQL_BootCamp_NativeBackup_MultiFile.sql
@@ -1,6 +1,6 @@
-BACKUP DATABASE TALab TO 
-DISK='\\cohesity-01.talabs.local\SQLView\TALab_01.BAK',
-DISK='\\cohesity-01.talabs.local\SQLView\TALab_02.BAK',
-DISK='\\cohesity-01.talabs.local\SQLView\TALab_03.BAK',
-DISK='\\cohesity-01.talabs.local\SQLView\TALab_04.BAK'
+BACKUP DATABASE CALab TO 
+DISK='\\cohesity-a.cohesitylabs.az\SQLView\TALab_01.BAK',
+DISK='\\cohesity-a.cohesitylabs.az\SQLView\TALab_02.BAK',
+DISK='\\cohesity-a.cohesitylabs.az\SQLView\TALab_03.BAK',
+DISK='\\cohesity-a.cohesitylabs.az\SQLView\TALab_04.BAK'
 WITH STATS=5
