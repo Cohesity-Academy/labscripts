fa8e1a24960b50a670f7e4b059033695eafa8775
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-05-15 14:33:20 -0400

    Update SQL_BootCamp_NativeRestore.sql

--- AA/sql/SQL_BootCamp_NativeRestore.sql
+++ AA/sql/SQL_BootCamp_NativeRestore.sql
@@ -2,17 +2,17 @@
 --RECONFIGURE
 --EXEc master.dbo.sp_configure 'xp_cmdshell', 1
 --RECONFIGURE
---execute xp_cmdshell 'dir \\cohesity-01.talabs.local\SQLView\*.*'
+--execute xp_cmdshell 'dir \\cohesity-a.cohesitylabs.az\SQLView\*.*'
 
 -- Step ONE, restore the database.
-RESTORE DATABASE TALab FROM DISK='\\cohesity-01.talabs.local\SQLView\TALab.BAK' WITH STATS=5, NORECOVERY
+RESTORE DATABASE CALab FROM DISK='\\cohesity-a.cohesitylabs.az\SQLView\CALab.BAK' WITH STATS=5, NORECOVERY
 
 -- Step TWO, restore the first log.
-RESTORE LOG TALab FROM DISK='\\cohesity-01.talabs.local\SQLView\TALab01.trn' WITH STATS=5, NORECOVERY
+RESTORE LOG CALab FROM DISK='\\cohesity-a.cohesitylabs.az\SQLView\CALab01.trn' WITH STATS=5, NORECOVERY
 
 -- Step THREE, restore the second log.
-RESTORE LOG TALab FROM DISK='\\cohesity-01.talabs.local\SQLView\TALab02.trn' WITH STATS=5, RECOVERY
+RESTORE LOG CALab FROM DISK='\\cohesity-a.cohesitylabs.az\SQLView\CALab02.trn' WITH STATS=5, RECOVERY
 
 -- Verify the database is open and accessible, AND the table exists.
-USE TALab
+USE CALab
 select * from MYLAB01
