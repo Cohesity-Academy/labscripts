98c4e8e1bf714b6d84a047a43c224dc600fdfab3
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-09-29 15:42:39 -0400

    Update appstarter.ps1

--- AA/appstarter.ps1
+++ AA/appstarter.ps1
@@ -4,17 +4,9 @@ param (
     [Parameter()][string]$vip = 'cohesity-a.cohesitylabs.az',  # endpoint to connect to
     [Parameter()][string]$username = 'admin',  # username for authentication / password storage
     [Parameter()][string]$domain = 'local',  # local or AD domain
-    [Parameter()][switch]$useApiKey,  # use API key authentication
     [Parameter()][string]$password = 'cohesity123',  # send password / API key via command line (not recommended)
-    [Parameter()][switch]$noPrompt,  # do not prompt for password
-    [Parameter()][switch]$mcm,  # connect to MCM endpoint
-    [Parameter()][string]$mfaCode = $null,  # MFA code
-    [Parameter()][switch]$emailMfaCode,  # email MFA code
     [Parameter()][string]$clusterName = $null,  # cluster name to connect to when connected to Helios/MCM
-    [Parameter()][array]$pause,
-    [Parameter()][array]$resume,
-    [Parameter()][array]$terminate,
-    [Parameter()][switch]$wait
+    [Parameter()][string]$targetApp = 'Spotlight' # TargetApps: ClamAV, Insight or Spotlight
 )
 
 # source the cohesity-api helper code
@@ -23,45 +15,92 @@ param (
 # authenticate
 apiauth -vip $vip -username $username -password $password -domain $domain -apiKeyAuthentication $useApiKey -mfaCode $mfaCode -sendMfaCode $emailMfaCode -heliosAuthentication $mcm -regionid $region -tenant $tenant -noPromptForPassword $noPrompt
 
-Write-Output "Getting views on Cohesity-A cluster ..." 
+Write-Output "Getting views on $vip cluster ..." 
 $views_json = api get views
 
-Write-Output "Checking available apps on the Cohesity-A cluster ..." 
+Write-Output "Checking available apps on the $vip cluster ..." 
 $installed_apps_json = api get apps 
 
-Write-Output "Starting Insight Cohesity-A cluster ..."
+Write-Output "Starting $targetApp $vip cluster ..."
 $insight_view = ($views_json.views.name -like "*insight*")
 $insight_view_id = ($views_json.views |?{$_.name -like "*insight*"}).viewid
-$insight_view_id = $insight_view_id
+[int]$insight_view_id = $insight_view_id
  
-$app_uid = ($installed_apps_json |?{$_.metadata.name -eq "Insight"}).appId
-$version = ($installed_apps_json |?{$_.metadata.name -eq "Insight"}).version
+$app_uid = ($installed_apps_json |?{$_.metadata.name -eq "$targetApp"}).appId
+$version = ($installed_apps_json |?{$_.metadata.name -eq "$targetApp"}).version
 $guid = New-Guid
 
-$insight_view_id.GetType()
 
-$myObject = @{ 
-    "creationUid" = "$guid";
-    "appUid" = $app_uid;
-    "appVersion" = $version;
-    "description" = "";
-    "settings" = @{
-    "vmNumReplicasList" = @();
-    "qosTier" = "kLow";
-    "protectedObjectPrivileges" = @{
-        "protectedObjectPrivilegesType" = "kNone"
-        };
-        "readViewPrivileges" = @{
-        "privilegesType" = "kSpecific";
-        "viewIds" = @($insight_view_id)
-        };
-        "readWriteViewPrivileges" = @{
-        "privilegesType" = "kSpecific";
-        "viewIds" = @()
+Function StartClamAV {
+Write-Output "$app_uid"
+    $myObject = @{ 
+        "creationUid" = "$guid";
+        "appUid" = $app_uid;
+        "appVersion" = $version;
+        "description" = "";
+        "settings" = @{
+            "vmNumReplicasList" = @();
+            "qosTier" = "kLow";
+            "protectedObjectPrivileges" = @{
+                "protectedObjectPrivilegesType" = "kNone"
+            }
         }
     }
+    api post appInstances $myObject
 }
-          
+
+Function StartSpotlight {
+Write-Output "$app_uid"
+    $myObject = @{ 
+        "creationUid" = "$guid";
+        "appUid" = $app_uid;
+        "appVersion" = $version;
+        "description" = "";
+        "settings" = @{
+        "vmNumReplicasList" = @();
+        "qosTier" = "kLow";
+        "protectedObjectPrivileges" = @{
+            "protectedObjectPrivilegesType" = "kNone"
+            }
+        }
+    }
+    api post appInstances $myObject
+}
+
+Function StartInsight {
+    $myObject = @{ 
+        "creationUid" = "$guid";
+        "appUid" = $app_uid;
+        "appVersion" = $version;
+        "description" = "";
+        "settings" = @{
+        "vmNumReplicasList" = @();
+        "qosTier" = "kLow";
+        "protectedObjectPrivileges" = @{
+            "protectedObjectPrivilegesType" = "kNone"
+            };
+            "readViewPrivileges" = @{
+            "privilegesType" = "kSpecific";
+            "viewIds" = @($insight_view_id)
+            };
+            "readWriteViewPrivileges" = @{
+            "privilegesType" = "kSpecific";
+            "viewIds" = @()
+            }
+        }
+    }
+    api post appInstances $myObject
+}
 
 
-api post appInstances $myObject
+if ($targetApp -eq "ClamAV") {
+    StartClamAV
+} elseif ($targetApp -eq "Spotlight") {
+    StartSpotlight
+} elseif ($targetApp -eq "Insight") {
+    StartInsight
+} else {
+    Write-Output "`nUnsupported target app: $targetApp" | Out-File $clog -Append
+}
+
+
