e32fb483e10bb3e5a8d8ecbd65d7213d13aafb0e
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-10-27 14:20:58 -0500

    Update disablemfa.ps1

--- AA/disablemfa.ps1
+++ AA/disablemfa.ps1
@@ -21,68 +21,36 @@ $cohesityIncarnationId = $response.incarnationId
 
 $payload1 = @{
     "clusterId" = [int64]$cohesityClusterId;
-    "serviceName" = "magneto";
+    "serviceName" = "iris";
     "gflags" = @(
         @{
-            "name" = "magneto_master_ignore_oob_copy_for_archival";
+            "name" = "mfa_enabled_for_AD_users";
             "value" = "false";
-            "reason" = "archive replica";
+            "reason" = "disable MFA for AD";
         }
     );
 } 
 
 $response1 = api POST /nexus/cluster/update_gflags $payload1
 
-
-
 $payload2 = @{
     "clusterId" = [int64]$cohesityClusterId;
-    "serviceName" = "magneto";
+    "serviceName" = "iris";
     "gflags" = @(
         @{
-            "name" = "magneto_master_restrict_inactive_job_update";
+            "name" = "iris_ui_flags";
             "value" = "false";
-            "reason" = "archive replica";
+            "reason" = "disable MFA for AD";
         }
     );
 }
 
 $response2 = api POST /nexus/cluster/update_gflags $payload2
 
-
-$payload3 = @{
-    "clusterId" = [int64]$cohesityClusterId;
-    "serviceName" = "bridge";
-    "gflags" = @(
-        @{
-            "name" = "icebox_allow_archive_now_archives_as_reference_and_base";
-            "value" = "true";
-            "reason" = "archive replica";
-        }
-    );
-} 
-$response3 = api POST /nexus/cluster/update_gflags $payload3
-
-
-$payload4 = @{
-    "clusterId" = [int64]$cohesityClusterId;
-    "serviceName" = "iris";
-    "gflags" = @(
-        @{
-            "name" = "iris_ui_flags";
-            "value" = "editRunForInactiveJobsEnabled=true";
-            "reason" = "archive replica";
-        }
-    );
-}
-$response4 = api POST /nexus/cluster/update_gflags $payload4
-
-
 $payload5 = @{
     "clusterId" = [int64]$cohesityClusterId;
     "services" = @(
         "iris"
-        "magneto"
     );
 } 
 $response5 = api POST /nexus/cluster/restart $payload5
