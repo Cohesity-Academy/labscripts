19a08ae782e30795ddf84290d321f4ba85431025
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-12-12 14:34:44 -0600

    Update secureshell.ps1

--- AA/secureshell.ps1
+++ AA/secureshell.ps1
@@ -13,9 +13,16 @@ Install-Module -Name Posh-SSH -RequiredVersion 3.0.8 -scope Allusers -Force
 $SessionSSH = New-SSHSessions -ComputerName $ip -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "$username", (ConvertTo-SecureString -AsPlainText "$password" -Force)) -AcceptKey
 Get-SSHSession | fl
 $session = Get-SSHSession -Index 0
+Start-Sleep 1
 $stream = $session.Session.CreateShellStream("dumb", 0, 0, 0, 0, 1000)
+Start-Sleep 1
 $stream.Write("iris_cli`n")
+Start-Sleep 1
 $stream.Write("admin`n")
+Start-Sleep 1
 $stream.Write("cohesity123`n")
+Start-Sleep 1
 $stream.Write("iris_cli`n")
+Start-Sleep 1
 $stream.Write("cluster secure-shell enable=false`n")
+Remove-SSHSession -SessionId 0
