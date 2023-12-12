4e64da05d679c322a98c03c4706889e70e60b1b7
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-12-12 14:18:08 -0600

    Update secureshell.ps1

--- AA/secureshell.ps1
+++ AA/secureshell.ps1
@@ -3,33 +3,19 @@
 # process commandline arguments
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $True)][string]$vip,  # the cluster to connect to (DNS name or IP)
-    [Parameter(Mandatory = $True)][string]$username,  # username (local or AD)
-    [Parameter(Mandatory = $True)][string]$password # local or AD domain password
+    [Parameter(Mandatory = $True)][string]$ip,  # ip of the cluster to connect to
+    [Parameter(Mandatory = $True)][string]$username,  # support username
+    [Parameter(Mandatory = $True)][string]$password # support password
 )
 
 # source the cohesity-api helper code
-. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
-
-# authenticate
-apiauth -vip $vip -username $username -domain $domain -password $password -quiet
-
-$response = api GET cluster
-$cohesityClusterId = $response.id
-$cohesityIncarnationId = $response.incarnationId
-
-
-$payload1 = @{
-    "clusterId" = [int64]$cohesityClusterId;
-    "serviceName" = "secure-shell";
-    "gflags" = @(
-        @{
-            "name" = "enable";
-            "value" = "false";
-        }
-    );
-} 
-
-$response1 = api POST /nexus/cluster/update_gflags $payload1
-
-Write-Output "`nAll Gflags have been set and service restarts are complete"
+Install-Module -Name Posh-SSH -RequiredVersion 3.0.8 -scope Allusers -Force
+$SessionSSH = New-SSHSessions -ComputerName $ip -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "$username", (ConvertTo-SecureString -AsPlainText "$password" -Force)) -AcceptKey
+Get-SSHSession | fl
+$session = Get-SSHSession -Index 0
+$stream = $session.Session.CreateShellStream("dumb", 0, 0, 0, 0, 1000)
+$stream.Write("iris_cli`n")
+$stream.Write("admin`n")
+$stream.Write("cohesity123`n")
+$stream.Write("iris_cli`n")
+$stream.Write("cluster secure-shell enable=false`n")
