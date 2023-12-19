0f0bb2dceed70aa4685eeae44996e76def2e97a4
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2023-12-19 09:05:57 -0600

    Update secureshell.ps1

--- AA/secureshell.ps1
+++ AA/secureshell.ps1
@@ -13,18 +13,25 @@ $SessionSSH = New-SSHSession -AcceptKey -ComputerName $ip -Credential (New-Objec
 Get-SSHSession | fl
 $session = Get-SSHSession -Index 0
 Start-Sleep 3
+$stream.read()
 $stream = $session.Session.CreateShellStream("dumb", 0, 0, 0, 0, 1000)
 Start-Sleep 3
+$stream.read()
 $stream.Write("iris_cli`n")
 Start-Sleep 3
+$stream.read()
 $stream.Write("admin`n")
 Start-Sleep 3
+$stream.read()
 $stream.Write("cohesity123`n")
 Start-Sleep 3
+$stream.read()
 $stream.Write("cluster secure-shell enable=false`n")
 start-sleep 3
+$stream.read()
 $stream.Write("exit`n")
 start-sleep 3
+$stream.read()
 $stream.Write("exit`n")
 Remove-SSHSession -SessionId 0
 Remove-SSHTrustedHost -HostName $ip
