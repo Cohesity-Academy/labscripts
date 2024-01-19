e8c14d092100dcf328ab79747a492fa6c9d7fe61
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2024-01-19 14:56:19 -0600

    Update protectNASnoindex.ps1

--- PA660/protectNASnoindex.ps1
+++ PA660/protectNASnoindex.ps1
@@ -6,27 +6,19 @@ param (
     [Parameter(Mandatory = $True)][string]$vip,  # the cluster to connect to (DNS name or IP)
     [Parameter(Mandatory = $True)][string]$username,  # username (local or AD)
     [Parameter(Mandatory = $True)][string]$password = '',  # local or AD domain password
-    [Parameter()][string]$domain = 'local',  # local or AD domain username
-    [Parameter()][array]$servers = '',  # optional name of one server protect
-    [Parameter()][string]$serverList = '',  # optional textfile of servers to protect
-    [Parameter()][array]$inclusions = '', # optional paths to exclude (comma separated)
-    [Parameter()][string]$inclusionList = '',  # optional list of exclusions in file
-    [Parameter()][array]$exclusions = '',  # optional name of one server protect
-    [Parameter()][string]$exclusionList = '',  # required list of exclusions
-    [Parameter(Mandatory = $True)][string]$jobName,  # name of the job to add server to
-    [Parameter()][switch]$skipNestedMountPoints,  # 6.3 and below - skip all nested mount points
-    [Parameter()][array]$skipNestedMountPointTypes = @(),  # 6.4 and above - skip listed mount point types
-    [Parameter()][switch]$replaceRules,
-    [Parameter()][switch]$allServers,
-    [Parameter()][string]$metadataFile = '',
-    [Parameter()][string]$startTime = '20:00',
-    [Parameter()][string]$timeZone = 'America/New_York',
-    [Parameter()][int]$incrementalSlaMinutes = 60,
-    [Parameter()][int]$fullSlaMinutes = 120,
+    [Parameter()][string]$jobName = '',  # name of protection job
+    [Parameter()][array]$nas = '',  # optional name of one nas source
     [Parameter()][string]$storageDomainName = 'DefaultStorageDomain',
-    [Parameter()][string]$policyName,
-    [Parameter()][ValidateSet('kBackupHDD', 'kBackupSSD')][string]$qosPolicy = 'kBackupHDD'
+    [Parameter()][string]$policyName = ''
 )
+
+### source the cohesity-api helper code
+. $(Join-Path -Path $PSScriptRoot -ChildPath cohesity-api.ps1)
+Import-Module $(Join-Path -Path $PSScriptRoot -ChildPath userRights.psm1)
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -domain $domain
+
 
 $myObject = @{
     "policyId" = "8158516650510261:1575649096260:3";
@@ -49,7 +41,7 @@ $myObject = @{
     "qosPolicy" = "kBackupHDD";
     "abortInBlackouts" = $false;
     "storageDomainId" = 3203;
-    "name" = "SMB-NAS";
+    "name" = "$jobName";
     "environment" = "kGenericNas";
     "isPaused" = $false;
     "description" = "";
