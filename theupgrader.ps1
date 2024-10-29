fa21e4929ef53149733f99d7ff8f5f2331d69ede
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2024-10-29 16:21:13 -0500

    Create theupgrader.ps1

---
+++ theupgrader.ps1
@@ -0,0 +1,16 @@
+#$Source = "theupgrader.ps1"  
+#$Destination = "theupgrader.ps1"  
+#Invoke-WebRequest -Uri $source -OutFile $Destination  
+
+Install-Script -Name winget-install -Force
+winget-install
+start-sleep 10
+$wgetpath = dir$ "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+TAKEOWN /F $wgetpath.fullname /R /A /D Y
+ICACLS $wgetpath.fullname /grant Administrators:F /T
+winget upgrade winscp --silent --accept-package-agreements --accept-source-agreements
+winget upgrade putty --silent --accept-package-agreements
+winget upgrade wireshark --silent --accept-package-agreements
+winget upgrade firefox --silent --accept-package-agreements
+# winget upgrade chrome --silent --accept-package-agreements
+# winget upgrade "Microsoft SQL Server Management Studio" --silent --accept-package-agreements
