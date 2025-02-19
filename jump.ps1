870f930894a65fa152429486c2b2eaa58ade0b90
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2025-02-19 11:29:58 -0600

    Update jump.ps1

--- jump.ps1
+++ jump.ps1
@@ -1,8 +1,13 @@
 $path = "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp"
 Set-ItemProperty -Path $path -Name UserAuthentication -Type DWord -Value 0
-shutdown /r /t 0
-
+$nic = get-netadapter
+Set-DnsClientServerAddress -InterfaceIndex $nic.ifIndex -ServerAddresses ("192.168.1.10")
 $Source = "https://github.com/Cohesity-Academy/labscripts/raw/refs/heads/main/theupgrader.ps1"
-$Destination = "theupgrader.ps1"
+$Destination = "c:\users\coh-student.cohesitylabs\documents\scripts\theupgrader.ps1"
 Invoke-WebRequest -Uri $source -OutFile $Destination
-./theupgrader.ps1
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File c:\users\coh-student.cohesitylabs\documents\scripts\theupgrader.ps1"
+$trigger = New-ScheduledTaskTrigger -AtLogon
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+$principal = New-ScheduledTaskPrincipal -UserId “NT AUTHORITY\SYSTEM” -LogonType Password
+Register-ScheduledTask -TaskName “Upgrade Software" -Action $action -Trigger $trigger -Principal $principal -Settings $settings
+shutdown /r /t 0
