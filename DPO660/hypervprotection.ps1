a3446db57e7d18cfdaba31dc1713fb471970db76
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2026-01-14 16:48:44 -0600

    Update hypervprotection.ps1

--- DPO660/hypervprotection.ps1
+++ DPO660/hypervprotection.ps1
@@ -7,6 +7,7 @@ param (
     [Parameter(Mandatory = $True)][string]$username,  # username (local or AD)
     [Parameter(Mandatory = $True)][string]$password,  # local or AD domain password
     [Parameter()][array]$server = '',  # optional name of one server protect
+    [Parameter(Mandatory = $true)][string[]]$vms = '',
     [Parameter(Mandatory = $True)][string]$name,  # name of policy
     [Parameter()][string]$startTime = '20:00',
     [Parameter()][string]$timeZone = 'America/New_York',
@@ -54,42 +55,54 @@ apiauth -vip $vip -username $username -domain $domain -password $password -quiet
     }
     
     # get HyperV Server ID
-    $hyperv = api get protectionSources
-    $targetName = $server
-    $hypervid = ($hyperv.protectionSource | Where-Object {$_.name -ieq $targetName}).id
-$hypervid$
-    
-    # get HyperV VMs
-    $vms = api get protectionSources
-    if($vms -is [array]){
-            $vm = $vms | Where-Object { $_.name -ieq $Server }
-            if (!$viewBox) { 
-                write-host "HyperV Server $Server not Found" -ForegroundColor Yellow
-                exit
-            }
-    }else{
-        $vm = $vms[0]
+    $protectionSources = api get protectionSources
+    $hypervId = ($protectionSources.protectionSource |Where-Object { $_.name -eq $server }).id
+    if (-not $hypervId) {
+        throw "HyperV server '$server' not found"
     }
 
+    # Normalize VM input
+    $vms = $vms -join ',' -split '\s*,\s*' |
+       Where-Object { $_ -and $_.Trim() } |
+       ForEach-Object { $_.Trim() }
+    
+    # get HyperV VMs
+    $vmSources = $protectionSources.nodes.protectionSource | Where-Object { $_.parentId -eq $hypervId }
+    $vmMatches = $vmSources | Where-Object { $_.name -in $vms }
+
+    # Validate VM existence
+    $missingVMs = $vms | Where-Object { $_ -notin $vmMatches.name }
+    if ($missingVMs) {
+        throw "VM(s) not found under HyperV server '$server': $($missingVMs -join ', ')"
+    }
+    $hypervObjects = @(
+        $vmMatches | ForEach-Object {
+            @{
+                "id" = $_.id
+                "isAutoprotected" = $false
+            }
+        }
+    )
+    
     
 $myObject = @{
     "policyId" = $policy.id;
     "startTime" = @{
-            "hour" = [int]$hour;
-            "minute" = [int]$minute;
-            "timeZone" = $timeZone
-                  };
+        "hour"   = [int]$hour;
+        "minute" = [int]$minute;
+        "timeZone" = $timeZone
+    };
     "priority" = "kMedium";
     "sla" = @(
-                @{
-                    "backupRunType" = "kFull";
-                    "slaMinutes" = 120
-                };
-                @{
-                    "backupRunType" = "kIncremental";
-                    "slaMinutes" = 60
-                }
-            );
+        @{
+            "backupRunType" = "kFull";
+            "slaMinutes" = 120
+        };
+        @{
+            "backupRunType" = "kIncremental";
+            "slaMinutes" = 60
+        }
+    );
     "qosPolicy" = "kBackupHDD";
     "abortInBlackouts" = $false;
     "storageDomainId" = $viewBox.id;
@@ -99,63 +112,42 @@ $myObject = @{
     "pausedNote" = "";
     "description" = "";
     "alertPolicy" = @{
-                        "backupRunStatus" = @(
-                                                "kFailure";
-                                                "kSlaViolation"
-                                            );
-                        "alertTargets" = @(
-
-                                         )
-                    };
+        "backupRunStatus" = @("kFailure"; "kSlaViolation");
+        "alertTargets" = @()
+    };
     "hypervParams" = @{
-                         "objects" = @(
-                                         @{
-                                             "id" = 17;
-                                             "isAutoprotected" = $false
-                                         };
-                                         @{
-                                             "id" = 18;
-                                             "isAutoprotected" = $false
-                                         }
-                                     );
-                         "excludeObjectIds" = @(
-
-                                              );
-                         "vmTagIds" = @(
-
-                                      );
-                         "excludeVmTagIds" = @(
-
-                                             );
-                         "protectionType" = "kAuto";
-                         "appConsistentSnapshot" = $false;
-                         "fallbackToCrashConsistentSnapshot" = $true;
-                         "indexingPolicy" = @{
-                                                "enableIndexing" = $true;
-                                                "includePaths" = @(
-                                                                     "/"
-                                                                 );
-                                                "excludePaths" = @(
-                                                                     "/$Recycle.Bin";
-                                                                     "/Windows";
-                                                                     "/Program Files";
-                                                                     "/Program Files (x86)";
-                                                                     "/ProgramData";
-                                                                     "/System Volume Information";
-                                                                     "/Users/*/AppData";
-                                                                     "/Recovery";
-                                                                     "/var";
-                                                                     "/usr";
-                                                                     "/sys";
-                                                                     "/proc";
-                                                                     "/lib";
-                                                                     "/grub";
-                                                                     "/grub2";
-                                                                     "/opt";
-                                                                     "/splunk"
-                                                                 )
-                                            }
-                     }
+        "objects" = $hypervObjects;   # <--- HERE
+        "excludeObjectIds" = @();
+        "vmTagIds" = @();
+        "excludeVmTagIds" = @();
+        "protectionType" = "kAuto";
+        "appConsistentSnapshot" = $false;
+        "fallbackToCrashConsistentSnapshot" = $true;
+        "indexingPolicy" = @{
+            "enableIndexing" = $true;
+            "includePaths" = @("/");
+            "excludePaths" = @(
+                "/$Recycle.Bin";
+                "/Windows";
+                "/Program Files";
+                "/Program Files (x86)";
+                "/ProgramData";
+                "/System Volume Information";
+                "/Users/*/AppData";
+                "/Recovery";
+                "/var";
+                "/usr";
+                "/sys";
+                "/proc";
+                "/lib";
+                "/grub";
+                "/grub2";
+                "/opt";
+                "/splunk"
+            )
+        }
+    }
 }
 
+$myObject | ConvertTo-Json -Depth 20
 $newprol = api post -v2 data-protect/protection-groups $myObject
