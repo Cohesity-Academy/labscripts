21c570495c7df1e3ff2cf58b2152f6710da7c2f5
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2026-01-15 14:08:47 -0600

    Create validateguest1.ps1

---
+++ DPO660/validateguest1.ps1
@@ -0,0 +1,25 @@
+### usage: ./validation.ps1 -vip mycluster -username myusername -passsword 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$objects = api get -v2 data-protect/protection-groups
+
+$pgs = ($objects.protectionGroups | Where-Object name -eq "VirtualProtection")
+
+if ($pgs.hypervParams.objects.name -contains "Guest-VM-1") {
+    Write-Host "Correct"
+} else {
+    Write-Host "Incorrect"
+}
