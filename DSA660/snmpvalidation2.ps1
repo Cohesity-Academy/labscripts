a65fa762a9aafa9796c331f2ff751869e4d8dbea
Cohesity-Academy <106822318+Cohesity-Academy@users.noreply.github.com>
2026-08-10 16:42:45 -0500

    Add files via upload

---
+++ DSA660/snmpvalidation2.ps1
@@ -0,0 +1,25 @@
+### usage: ./snmpvalidation2.ps1 -vip mycluster -username myusername -passsword 
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$password
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -password $password -quiet
+
+$objects = api get /snmp/config
+
+If ($objects.server -eq "192.168.1.90" -and
+    $objects.trapUser.userName -eq "cohesityV2Trap" -and
+    $objects.version -eq "kSnmpV2" -and
+    $objects.readUser.userName -eq "cohesityV2Public") {
+Write-Host "Correct"
+}
+Else {Write-Host "Incorrect"}
